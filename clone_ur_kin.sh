d82801d9192417b94edd9f461ab0d4eb67ebb7c5
Ahmad Abouelainein <ahmad.abouelainein@gmail.com>
2025-07-21 13:45:41 +0000

    simplify ur kin clone concept

---
+++ clone_ur_kin.sh
@@ -0,0 +1,6 @@
+git clone --depth 1 --filter=blob:none --sparse \
+          --branch noetic-devel \
+          https://github.com/ros-industrial/universal_robot.git
+
+cd universal_robot
+git sparse-checkout set ur_kinematics
